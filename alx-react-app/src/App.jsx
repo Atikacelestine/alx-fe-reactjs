e2b77889a7724910ef26744ef8d7a9424422975f
Atikacelestine <celestineatika313@gmail.com>
2024-11-01 16:38:56 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -44,6 +44,9 @@ function App() {
       <div>
         <Footer />
       </div>
+      <div>
+              <UserProfile name="Alice" age ="25" bio= "Loves hiking and photography" />
+</div>
     </>
   )
 }
