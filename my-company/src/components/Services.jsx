ef6eb7ef3205ad8996cf7d91445da42ae384aca5
Atikacelestine <celestineatika313@gmail.com>
2024-11-10 23:57:01 +0300

    scent

---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,18 @@
+function Services() {
+    return (
+      <div style={{ padding: '20px',backgroundColor: '#f9f9f9',
+        padding:'20px',
+        margin:'15px 0',
+        borderRadius:'8px',
+        boxShadow: '0 4px 8px rgba (0,0,0.1)' }}>
+        <h1>Our Services</h1>
+        <ul>
+          <li>Technology Consulting</li>
+          <li>Market Analysis</li>
+          <li>Product Development</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Services;
