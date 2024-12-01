8eb2462ef5b87d85b46180e038b38f20ce9501b5
Atikacelestine <celestineatika313@gmail.com>
2024-12-01 18:04:18 +0300

    scent

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,7 +8,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-36 h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
+      <h1 className="text-lg md:text-xl text-sm text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
       <p className="text-gray-600 text-base text-center">
