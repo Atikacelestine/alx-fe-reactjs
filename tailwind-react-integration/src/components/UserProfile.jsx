b9eb8dc2d07c4377b627074668022049ebf9d7d0
Atikacelestine <celestineatika313@gmail.com>
2024-12-01 18:06:07 +0300

    scent

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -15,7 +15,7 @@ function UserProfile() {
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
-        <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+        <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out sm:w-24 sm:h-24 md:w-36 md:h-36">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
