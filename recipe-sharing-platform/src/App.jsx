4b71b2ccd76589d368caf489064e46b12f58d4d6
Atikacelestine <celestineatika313@gmail.com>
2024-12-07 11:57:08 +0300

    scent

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -5,7 +5,10 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
- 
+ import AddRecipeForm from "./components/AddRecipeForm";
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +20,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/recipe/:id" element={<RecipeDetail />} />
+     <Route path="/add-recipe" element={<AddRecipeForm />} />
     </Routes>
   </Router>
       <div>
