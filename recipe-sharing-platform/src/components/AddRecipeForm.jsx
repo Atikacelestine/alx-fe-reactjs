968b7b51ffc7c83b749f11f00369d4571b8a4227
Atikacelestine <celestineatika313@gmail.com>
2024-12-07 12:19:03 +0300

    Update

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -10,7 +10,7 @@ const AddRecipeForm = () => {
   const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value } = e.target.value;
     setFormData({ ...formData, [name]: value });
   };
 
