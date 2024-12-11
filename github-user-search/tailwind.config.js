e224708acaa4c25497dd585e521768acdb4e79a9
Atikacelestine <celestineatika313@gmail.com>
2024-12-11 20:41:16 +0300

    scent

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Include Vite's HTML entry point
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
